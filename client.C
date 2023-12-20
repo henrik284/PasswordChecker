4db0f3fb75a9caa4be1cb05b546eebbfec2ff50b
henrik <henrik.boeckenfoerde@hsbi.de>
2023-12-20 19:07:24 +0100

    rpetition of password checking added. (For result statitic)

--- client.C
+++ client.C
@@ -33,7 +33,7 @@ int main(){
             }
         }
     }
-    cout << "Connected to the Server" << endl;
+    cout << "Connected to the Server" << endl << endl;
 
     //maxPwdLength & maxCharAmount abfragen
     int maxPwdLength;
@@ -43,79 +43,100 @@ int main(){
     int maxCharAmount;
     cout << "Please enter the maximum amount of available characters for the password: ";
     cin >> maxCharAmount;
-    cout << endl;
 
     //maxCharAmount should not be bigger actual number of available characters, which are stored in the TASK1::SYMBOLS
     if(maxCharAmount>TASK1::SYMBOLS.size()){
         maxCharAmount = TASK1::SYMBOLS.size();
     }
 
+    int repetitions;
+    cout << "How often should the analysis be repeated (max. 10)? ";
+    cin >> repetitions;
+    cout << endl;
+
+    //Limit number of repetitions to ten
+    if(repetitions>10){
+        repetitions = 10;
+    }
+
     //Create Stream to .csv file
     ofstream resultsStream("Results/Results.csv");
     if(!resultsStream){
         cout << "Output file could not be created. Pleas create a \"Results\" folder before starting the client." << endl;
+
+        //close the server
+        client.sendData("BYEBYE");
+
+        //end the client
+        return 0;
+
     } else{
-        cout << "Output file created" << endl;
+        cout << "Output file created" << endl << endl;
     }
-    sleep(1);
 
-    resultsStream << "password length; available characters; trys; time" << endl;
+    //write the header line in the results file
+    resultsStream << "repetition; password length; available characters; trys; time" << endl;
 
-    //Loop over every Number of available characters
-    for(int charsAvailable = 1; charsAvailable <= maxCharAmount; charsAvailable++){
+    //Repeat the analysis as often as specified with the repetitions input
+    for(int analysisNr = 0; analysisNr < repetitions; analysisNr++){
 
-        //Loop over all lenghts of passwords
-        for(int pwdLenght = 1; pwdLenght <= maxPwdLength; pwdLenght++){
+        cout << "ANALYSIS NUMBER: " << analysisNr << endl << endl;
 
-            //Loop over max 10 trys, in which the client trys to request a new password. Server has to answer with "ServerStatus(Password created)" to continue.
-            for(int i = 0; i < 10; i++){
+        //Loop over every Number of available characters
+        for(int charsAvailable = 1; charsAvailable <= maxCharAmount; charsAvailable++){
 
-                //Send "Create-New-Password"-Command to the Server
-                client.sendData(pwdMsg::newMsg("NewPassword", to_string(pwdLenght), to_string(charsAvailable)));
-                cout << "Sent message to server:\t" << pwdMsg::newMsg("NewPassword", to_string(pwdLenght), to_string(charsAvailable)) << endl;
+            //Loop over all lenghts of passwords
+            for(int pwdLenght = 1; pwdLenght <= maxPwdLength; pwdLenght++){
 
-                //Wait for the server to confirm, that a new password was created
-                pwdMsg recmsg(client.receive(MESSAGE_SIZE));
-                cout << "Answer from Server:\t" << recmsg.newMsg(recmsg.id_, recmsg.arg1_, recmsg.arg2_, recmsg.arg3_, recmsg.arg4_) << endl << endl;
+                //Loop over max 10 trys, in which the client trys to request a new password. Server has to answer with "ServerStatus(Password created)" to continue.
+                for(int i = 0; i < 10; i++){
 
-                //If Sever has created the password, end this loop.
-                if((recmsg.id_ == "ServerStatus") && (recmsg.arg1_ == "Password Created")){
-                    break;
-                //If not, try again after 1 second, if it isn't already the 10th try. Then stop programm
-                } else if (i == 9){
-                    cout << "Server can not create the password. Client stopped." << endl;
-                    return 0;
+                    //Send "Create-New-Password"-Command to the Server
+                    client.sendData(pwdMsg::newMsg("NewPassword", to_string(pwdLenght), to_string(charsAvailable)));
+                    cout << "Sent message to server:\t" << pwdMsg::newMsg("NewPassword", to_string(pwdLenght), to_string(charsAvailable)) << endl;
+
+                    //Wait for the server to confirm, that a new password was created
+                    pwdMsg recmsg(client.receive(MESSAGE_SIZE));
+                    cout << "Answer from Server:\t" << recmsg.newMsg(recmsg.id_, recmsg.arg1_, recmsg.arg2_, recmsg.arg3_, recmsg.arg4_) << endl << endl;
+
+                    //If Sever has created the password, end this loop.
+                    if((recmsg.id_ == "ServerStatus") && (recmsg.arg1_ == "Password Created")){
+                        break;
+                    //If not, try again after 1 second, if it isn't already the 10th try. Then stop programm
+                    } else if (i == 9){
+                        cout << "Server can not create the password. Client stopped." << endl;
+                        return 0;
+                    }
+                    sleep(1);
                 }
-                sleep(1);
+
+                //Create the initial Password
+                string password(pwdLenght, TASK1::SYMBOLS.at(0));
+
+                //Variable to store the number of trys until the password is hacked.
+                int counterOfTrials = 0;
+
+                //Variable which is set to true if the password is hacked.
+                bool passwordHacked = false;
+
+                //Set Start time point.
+                time_t timebuffer = time(NULL);
+
+                //Creating all possible Password with a call of the recursive createPwdRec function.
+                client.createPwdRec(password, pwdLenght-1, charsAvailable, counterOfTrials, passwordHacked, &client);
+
+                cout << "Counter of Trials: " << counterOfTrials << "\tFinally hacked? " << passwordHacked << endl << endl << endl;
+
+                //Save pwdLength, charsAvailable, CounterOfTrials and used time in .csv file
+                resultsStream << (analysisNr+1) << ";" << pwdLenght << ";" << charsAvailable << ";" << counterOfTrials << ";" << (int)(time(NULL)-timebuffer) << endl;
+
             }
-
-            //Create the initial Password
-            string password(pwdLenght, TASK1::SYMBOLS.at(0));
-
-            //Variable to store the number of trys until the password is hacked.
-            int counterOfTrials = 0;
-
-            //Variable which is set to true if the password is hacked.
-            bool passwordHacked = false;
-
-            //Set Start time point.
-            time_t timebuffer = time(NULL);
-
-            //Creating all possible Password with a call of the recursive createPwdRec function.
-            client.createPwdRec(password, pwdLenght-1, charsAvailable, counterOfTrials, passwordHacked, &client);
-
-            cout << endl << "Counter of Trials: " << counterOfTrials << "\tFinally hacked? " << passwordHacked << endl << endl << endl;
-
-            //Save pwdLength, charsAvailable, CounterOfTrials and used time in .csv file
-            resultsStream << pwdLenght << ";" << charsAvailable << ";" << counterOfTrials << ";" << (int)(time(NULL)-timebuffer) << endl;
-
         }
     }
-    if(!resultsStream){
-        cout << "Results could not be saved in the Results.csv file. Check if the directory of this program has a \"Results\" folder" << endl;
-    } else {
-        cout << "The Results of this analysis have been saved in the Results.csv file in the Results folder." << endl;
-    }
+
+    cout << "The Results of this analysis have been saved in the Results.csv file in the Results folder." << endl << endl;
+
+    //end Filestream
     resultsStream.close();
 
     //close the server
@@ -142,11 +163,11 @@ void pwdCheckerClient::createPwdRec(string& password, int index, int chars, int&
             counterOfTrials++;
 
             ptrClient->sendData(pwdMsg::newMsg("CheckPassword", password));
-            cout << "Sent message to server:\t" << pwdMsg::newMsg("CheckPassword", password) << endl << endl;
+            cout << "Sent message to server:\t" << pwdMsg::newMsg("CheckPassword", password) << endl;
 
             //Receive an answer of the server.
             pwdMsg recmsg(ptrClient->receive(MESSAGE_SIZE));
-            cout << "Answer from Server:\t" << recmsg.newMsg(recmsg.id_, recmsg.arg1_, recmsg.arg2_, recmsg.arg3_, recmsg.arg4_) << endl << endl;
+            //cout << "Answer from Server:\t" << recmsg.newMsg(recmsg.id_, recmsg.arg1_, recmsg.arg2_, recmsg.arg3_, recmsg.arg4_) << endl << endl;
 
                         //Simulation of Server answer (for testing the client without the server)
                         /*
